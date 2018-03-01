96e742c1223a25bbe5f87e0d56588897ce0f5573
Journeyer Joh <oosaprogrammer@gmail.com>
2018-03-01 16:56:44 +0900

    brought Singleton

---
+++ Singleton/Chocolate/driver/chocolate.cpp
@@ -0,0 +1,36 @@
+//===--- chocolate.cpp - ----------------------------------------*- C++ -*-===//
+//
+//                     Head First Design Patterns
+//
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief
+///
+//===----------------------------------------------------------------------===//
+
+#include "ChocolateBoiler.hpp"
+#include <iostream>
+
+using namespace HFDP::Factory::Singleton;
+
+ChocolateBoiler* ChocolateBoiler::_uniqueInstance = 0;
+
+int main( int argc, char* argv[] ) {
+
+  ChocolateBoiler* boiler = ChocolateBoiler::getInstance();
+  boiler->fill();
+  boiler->boil();
+  boiler->drain();
+
+  // will return the existing instance
+  ChocolateBoiler* boiler2 = ChocolateBoiler::getInstance();
+
+  if( boiler == boiler2 )
+    std::cout << "Got same boiler" << std::endl;
+  else
+    std::cout << "Oh oh! got a different boiler" << std::endl;
+
+  return 0;
+}
