0e38c9c98b6dd052a14089485f11d21cc9095735
Journeyer Joh <oosaprogrammer@gmail.com>
2018-03-07 15:26:09 +0900

    applied range-based for

--- Factory/Pizzafm/lib/Pizza.cpp
+++ Factory/Pizzafm/lib/Pizza.cpp
@@ -39,9 +39,8 @@ void Pizza::prepare() const
   std::cout << "Tossing dough..." << std::endl;
   std::cout << "Adding sauce..." << std::endl;
   std::cout << "Adding toppings: " << std::endl;
-  for( std::list< std::string >::iterator iterator = _toppings.begin();
-       _toppings.end() != iterator; ++iterator ) {
-    std::cout << "   " << iterator->c_str() << std::endl;
+  for( const std::string &topping : _toppings ) {
+    std::cout << "   " << topping.c_str() << std::endl;
   }
 }
 void Pizza::bake() const
@@ -72,9 +71,8 @@ std::string Pizza::toString() const
   value << "---- " << _name.c_str() << " ----" << std::endl;
   value << _dough.c_str() << std::endl;
   value << _sauce.c_str() << std::endl;
-  for( std::list< std::string >::iterator iterator = _toppings.begin();
-       _toppings.end() != iterator; ++iterator ) {
-    value << iterator->c_str() << std::endl;
+  for( const std::string &topping : _toppings ) {
+    value << topping.c_str() << std::endl;
   }
   return value.str();
 }
