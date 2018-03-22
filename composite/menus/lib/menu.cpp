d85306e737f33927e9fc526396a5a8b3ac56dae7
Journeyer Joh <oosaprogrammer@gmail.com>
2018-03-22 14:24:21 +0900

    indentation aligned

--- composite/menus/lib/menu.cpp
+++ composite/menus/lib/menu.cpp
@@ -29,55 +29,51 @@
 namespace headfirst {
 
 
-Menu::Menu( const std::string name, const std::string description ) :
-  name_( name ), description_( description )
-{
-  std::cout << "Menu::Menu" << std::endl;
-}
-void Menu::Add( MenuComponent* menu_component )
-{
-  assert( menu_component );
-  std::cout << "Menu::add" << std::endl;
-  menu_components_.push_back( menu_component );
-}
-void Menu::Remove( MenuComponent* menu_component )
-{
-  assert( menu_component );
-  std::cout << "Menu::remove" << std::endl;
-  ////////////////////////////////////////////////////////////
-  // This part is remained as a to-do
-  //std::Remove( menu_components_.begin(), menu_components_.end(),
-  //             menu_component->GetName().c_str());
-}
-MenuComponent* Menu::GetChild( int i ) const
-{
-  std::cout << "Menu::GetChild" << std::endl;
-  return menu_components_[i];
-}
-std::string Menu::GetName() const
-{
-  std::cout << "Menu::GetName" << std::endl;
-  return name_;
-}
-std::string Menu::GetDescription() const
-{
-  std::cout << "Menu::GetDescription" << std::endl;
-  return description_;
-}
-void Menu::Print() const
-{
-  std::cout << "Menu::print" << std::endl;
-  std::cout << std::endl << GetName().c_str();
-  std::cout << ", " << GetDescription().c_str() << std::endl;
-  std::cout << "---------------------" << std::endl;
-
-  //std::vector< MenuComponent* >::iterator
-  //  iterator = menu_components_.begin();
-  //while( iterator != menu_components_.end() ) {
-  //  MenuComponent> menu_component = *iterator++;
-  for ( MenuComponent* menu_component : menu_components_ ) {
-    menu_component->Print();
+  Menu::Menu( const std::string name, const std::string description ) :
+    name_( name ), description_( description )
+  {
+    std::cout << "Menu::Menu" << std::endl;
   }
-}
+  void Menu::Add( MenuComponent* menu_component )
+  {
+    assert( menu_component );
+    std::cout << "Menu::add" << std::endl;
+    menu_components_.push_back( menu_component );
+  }
+  void Menu::Remove( MenuComponent* menu_component )
+  {
+    assert( menu_component );
+    std::cout << "Menu::remove" << std::endl;
+    ////////////////////////////////////////////////////////////
+    // This part is remained as a to-do
+    //std::Remove( menu_components_.begin(), menu_components_.end(),
+    //             menu_component->GetName().c_str());
+  }
+  MenuComponent* Menu::GetChild( int i ) const
+  {
+    std::cout << "Menu::GetChild" << std::endl;
+    return menu_components_[i];
+  }
+  std::string Menu::GetName() const
+  {
+    std::cout << "Menu::GetName" << std::endl;
+    return name_;
+  }
+  std::string Menu::GetDescription() const
+  {
+    std::cout << "Menu::GetDescription" << std::endl;
+    return description_;
+  }
+  void Menu::Print() const
+  {
+    std::cout << "Menu::print" << std::endl;
+    std::cout << std::endl << GetName().c_str();
+    std::cout << ", " << GetDescription().c_str() << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+    for ( MenuComponent* menu_component : menu_components_ ) {
+      menu_component->Print();
+    }
+  }
 
 } //namespace headfirst
